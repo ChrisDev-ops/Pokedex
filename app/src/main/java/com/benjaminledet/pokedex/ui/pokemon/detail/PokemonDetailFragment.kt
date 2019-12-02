75b4b6a928b637744792944050eda736ae4af7a4
Christopher Elzea <lzc2998a@iut.iut-tlse3.fr>
2019-12-02 17:30:24 +0100

    Ajout du type

--- app/src/main/java/com/benjaminledet/pokedex/ui/pokemon/detail/PokemonDetailFragment.kt
+++ app/src/main/java/com/benjaminledet/pokedex/ui/pokemon/detail/PokemonDetailFragment.kt
@@ -1,15 +1,19 @@
 package com.benjaminledet.pokedex.ui.pokemon.detail
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.benjaminledet.pokedex.R
 import com.benjaminledet.pokedex.data.repository.utils.Status
+import com.benjaminledet.pokedex.ui.pokemon.list.PokemonsContainerFragment
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_pokemon_detail.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -22,6 +26,7 @@ class PokemonDetailFragment: Fragment() {
         return inflater.inflate(R.layout.fragment_pokemon_detail, container, false)
     }
 
+    @SuppressLint("StringFormatInvalid")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.pokemonId.postValue(arguments?.getInt("pokemonId"))
@@ -35,8 +40,27 @@ class PokemonDetailFragment: Fragment() {
             activity?.title = pokemon?.name
             weight.text = getString(R.string.pokemon_weight, pokemon?.detail?.weight.toString())
             height.text = getString(R.string.pokemon_height, pokemon?.detail?.height.toString())
+            type.text= getString(R.string.pokemon_type, pokemon?.detail?.types.toString())
+
 
            var typesPoke: String? = pokemon?.detail?.types?.get(0)
+            //val constrainL :ConstraintLayout = view.findViewById((R.id.cat_card_list_item_card),
+                val constrainL=cat_card_list_item_card
+//recup les couleur : .setbackgroundColor(requireContext().getColor(R.color.blue)
+//setBackgroundColor(requireContext().getAttributeFromAttr(R.attr.colorPrimary
+            when(typesPoke){
+                "fire" -> constrainL.setBackgroundColor(requireContext().getColor(R.color.fire))
+                "fire" -> constrainL.setBackgroundColor(requireContext().getColor(R.color.fire))
+                "fire" -> constrainL.setBackgroundColor(requireContext().getColor(R.color.fire))
+                "fire" -> constrainL.setBackgroundColor(requireContext().getColor(R.color.fire))
+                "fire" -> constrainL.setBackgroundColor(requireContext().getColor(R.color.fire))
+                "fire" -> constrainL.setBackgroundColor(requireContext().getColor(R.color.fire))
+                "fire" -> constrainL.setBackgroundColor(requireContext().getColor(R.color.fire))
+                "fire" -> constrainL.setBackgroundColor(requireContext().getColor(R.color.fire))
+                "fire" -> constrainL.setBackgroundColor(requireContext().getColor(R.color.fire))
+
+
+            }
 
 
 
